3f53b4221723858366b9c8d37adb9b127c2212c1
zsolt nemeth <furion1@gmail.com>
2016-12-01 18:24:12 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,121 @@
+import Exponent from 'exponent';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+    Dimensions
+} from 'react-native';
+import { TabViewAnimated, TabBarTop } from 'react-native-tab-view';
+import { Ionicons } from '@exponent/vector-icons';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+// Import the reducer and create a store
+import { reducer } from './src/postsRedux'
+
+
+import Feed from './src/Feed'
+
+
+// Add the thunk middleware to our store
+const store = createStore(reducer, applyMiddleware(thunk))
+
+const initialLayout = {
+    height: 0,
+    width: Dimensions.get('window').width,
+};
+
+
+
+class App extends React.Component {
+
+    static title = 'Icon only top bar';
+    static appbarElevation = 0;
+
+    static propTypes = {
+        style: View.propTypes.style,
+    };
+
+    state = {
+        index: 0,
+        routes: [
+            { key: '1', icon: 'md-restaurant' },
+            { key: '2', icon: 'md-bicycle' },
+            { key: '3', icon: 'md-color-palette' },
+        ],
+    };
+
+    _handleChangeTab = (index) => {
+        this.setState({
+            index,
+        });
+    };
+
+    _renderIcon = ({ route }) => {
+        return (
+            <Ionicons
+                name={route.icon}
+                size={24}
+                color='white'
+            />
+        );
+    };
+
+    _renderHeader = (props) => {
+        return (
+            <TabBarTop
+                {...props}
+                indicatorStyle={styles.indicator}
+                renderIcon={this._renderIcon}
+                style={styles.tabbar}
+            />
+        );
+    };
+
+    _renderScene = ({ route }) => {
+        switch (route.key) {
+            case '1':
+                return <Provider store={store}><Feed /></Provider>;
+            case '2':
+                return <View style={[ styles.page, { backgroundColor: '#673ab7' } ]} />;
+            case '3':
+                return <View style={[ styles.page, { backgroundColor: '#4caf50' } ]} />;
+            default:
+                return null;
+        }
+    };
+
+    render() {
+        return (
+            <TabViewAnimated
+                style={[ styles.container, this.props.style ]}
+                navigationState={this.state}
+                renderScene={this._renderScene}
+                renderHeader={this._renderHeader}
+                onRequestChangeTab={this._handleChangeTab}
+                initialLayout={initialLayout}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    tabbar: {
+        backgroundColor: '#222',
+    },
+    page: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    indicator: {
+        backgroundColor: '#ffeb3b',
+    },
+});
+
+Exponent.registerRootComponent(App);
